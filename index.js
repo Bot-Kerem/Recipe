1373c98f05e51212975d6be746a1ce4ef4f75fae
Bot-Kerem <akerem1434@gmail.com>
2022-09-24 00:13:16 +0300

    finished tree node

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ async function getJson(dataPath)
     const recipe_list = document.querySelector(".recipes");
     recipes.forEach(recipe => {
         var node = document.createElement('li');
+        var description = document.createElement('p');
+        description.appendChild(document.createTextNode("AAAAA"));
+        description.classList.add("nested");
         node.appendChild(document.createTextNode(recipe["Name"]));
+        node.appendChild(description);
         node.classList.add("recipe");
         node.addEventListener("click", function() 
         {
             node.classList.toggle("recipe-open");
+            const childs = node.childNodes;
+            for(let i = 1; i <= childs.length; i++)
+            {
+                childs[i].classList.toggle("active");
+            }
         })
         recipe_list.appendChild(node);
     });
